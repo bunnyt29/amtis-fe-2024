e49d4558851dde9e15cf5dda2472d44437200a20
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2024-06-01 16:28:36 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -6,18 +6,32 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle("open");
 });
 
-var dessertNames = document.querySelector('.dessert-cards-card-image');
-var dessertShortDescriptions = document.querySelector('.dessert-cards-card-image');
-var dessertLongDescriptions = document.querySelector('.dessert-cards-card-image');
-var dessertNames = document.querySelector('.dessert-cards-card-image');
+var dessertNames = document.querySelectorAll('.dessert-cards-card-name');
+var dessertShortDescriptions = document.querySelectorAll('.dessert-cards-card-description-short');
+var dessertLongDescriptions = document.querySelectorAll('.dessert-cards-card-description-long');
+var dessertIngrediants = document.querySelectorAll('.desserts-dessert-text-ingredients');
+var dessertImages = document.querySelectorAll('.dessert-cards-card-image');
 
 async function fetchData() {
     const response = await fetch("http://localhost:4000/desserts");
     const desserts = await response.json();
+
+    // for (let index = 0; index < dessertNames.length; index++) {
+    //     dessertNames[index].innerHTML = desserts[index].name;
+    // }
+
+    for (let index = 0; index < dessertShortDescriptions.length; index++) {
+        dessertShortDescriptions[index].innerHTML = desserts[index].description_short;
+    }
+
+    // for (let index = 0; index < dessertImages.length; index++) {
+    //     dessertImages[index].srcset = dessertImages[index].srcset + desserts[index].image;
+    //     console.log(dessertImages[index].srcset)
+    // }
+
+    for (let index = 0; index < dessertShortDescriptions.length; index++) {
+        dessertIngrediants[index].innerHTML = desserts[index].ingredients_text;
+    }
 }
 
 fetchData();
-
-for (let index = 0; index < dessertNames.length; index++) {
-    dessertNames[index].innerHTML = desserts[0].name;
-}
